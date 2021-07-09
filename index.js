419df99eda1eda2431303db3634fb16ccf73c9e7
SangHun <hepydotos@gmail.com>
2021-07-09 22:13:09 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const express = require('express');
 const app = express()
 const port = 5000
 const mongoose = require('mongoose');
+const {User} = require("./models/User");
+const config = require("./config/key");
 
-mongoose.connect('mongodb://localhost:27017/testdb', {
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req,res) => res.send('hello world'))
+
+app.post('/register', (req,res) => {
+    
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
